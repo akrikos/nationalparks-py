577fb817498b27cf8f3a838c318bb48cb3bdb863
Mark Merrill <mark.merrill@rackspace.com>
2018-02-10 18:12:22 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'I am the very model of a modern major general',
+        'displayName': 'I am the very model of a modern major general,',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
